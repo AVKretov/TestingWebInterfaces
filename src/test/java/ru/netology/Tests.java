3696d459cfac5496896f114d16a0f475a1a2557d
Aleksandr Kretov <kretov_a@outlook.com>
2022-04-30 00:40:15 +0300

    positive test fix

--- src/test/java/ru/netology/Tests.java
+++ src/test/java/ru/netology/Tests.java
@@ -20,14 +20,4 @@ class Tests {
                 "Наш менеджер свяжется с вами в ближайшее время."));
     }
 
-    @Test
-    void happyPath() {
-        open("http://localhost:9999");
-        $("[name=\"name\"]").setValue("Петров Василий");
-        $("[name=\"phone\"]").setValue("+79270000000");
-        $(".checkbox__box").click();
-        $(".button__text").click();
-        $(".paragraph_theme_alfa-on-white").shouldHave(exactText("Ваша заявка успешно отправлена! " +
-                "Наш менеджер свяжется с вами в ближайшее время."));
-    }
 }
